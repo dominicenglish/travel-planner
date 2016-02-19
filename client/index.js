877e5c7ffcbbb9b6a400d6ad7330eb555aef64f7
Dominic English <dominic.b.english@gmail.com>
2016-02-19 14:48:07 +1000

    feat(redux-saga): add basic redux-saga setup

--- client/index.js
+++ client/index.js
@@ -2,15 +2,21 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from '../shared/routes';
-import { createStore, combineReducers } from 'redux';
-import * as reducers from '../shared/reducers/index.js';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import * as reducers from '../shared/redux/reducers/index.js';
 import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+import sagas from '../shared/redux/sagas/sagas.js';
 require('../static/main.css');
 require('../static/bundle.css');
 
 const reduxState = window.__INITIAL_STATE__;
 // const reducer = combineReducers(reducers);
-const store = createStore(reducers.counter, reduxState);
+const store = createStore(
+  reducers.counter,
+  reduxState,
+  applyMiddleware(createSagaMiddleware(sagas)),
+);
 
 render((
   <Provider store={store}>
---
+++ shared/redux/sagas/sagas.js
@@ -0,0 +1,7 @@
+import { takeEvery, takeLatest } from 'redux-saga';
+import { call, put } from 'redux-saga/effects';
+import { ApiClient } from '../../../api/ApiClient.js';
+
+export default function *sagas() {
+
+}
