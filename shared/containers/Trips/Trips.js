7c27f3357a617657f3d1fe6d71b8909f5fec06e4
Dominic English <dominic.b.english@gmail.com>
2016-02-22 14:59:25 +1000

    dispatch TRIPS_GET on componentWillMount, add basic styles

--- shared/containers/Trips/Trips.js
+++ shared/containers/Trips/Trips.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import { TRIPS_GET } from '../../redux/actions/tripsActions.js';
+import defaultStyles from '../../styles/index.js';
+
 class Trips extends Component {
+
+  componentWillMount() {
+    const { dispatch } = this.props;
+    dispatch({type: TRIPS_GET});
+  }
+
   render() {
-    const trips = this.props.trips ? this.props.trips.map(trip => (<li>{trip.title}</li>)) : [];
+    const trips = this.props.trips ? this.props.trips.map(trip => (<li key={trip.id}>{trip.title}</li>)) : [];
     return (
-      <div>
+      <div className={styles.list}>
         <h2> List of trips </h2>
-        <ul>
-          <li> trip 1</li>
+        <ul className={styles.list}>
           {trips}
         </ul>
       </div>
@@ -17,3 +25,15 @@ class Trips extends Component {
 }
 
 export default connect(state => ({trips: state.trips}))(Trips);
+
+const styles = cssInJS({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  list: {
+    // ...defaultStyles.list,
+    flexBasis: '500px',
+    backgroundColor: 'rgba(200, 200, 200, 1)',
+  },
+});
