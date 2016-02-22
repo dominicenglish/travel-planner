a775f3542858d73aa03f8bdc7ee4217ee8d329d6
Dominic English <dominic.b.english@gmail.com>
2016-02-22 14:36:45 +1000

    add basic saga for fetching trips from the api

--- shared/redux/sagas/sagas.js
+++ shared/redux/sagas/sagas.js
@@ -1,7 +1,23 @@
 import { takeEvery, takeLatest } from 'redux-saga';
 import { call, put } from 'redux-saga/effects';
-import { ApiClient } from '../../../api/ApiClient.js';
+import { APIClient } from '../../../api/ApiClient.js';
+import {
+  TRIPS_GET,
+  TRIPS_GET_SUCCESS,
+  TRIPS_GET_FAIL
+} from '../actions/tripsActions.js';
 
-export default function *sagas() {
+const api = new APIClient();
 
+export function *getTrips() {
+  try {
+    const trips = yield api.get('/trips');
+    yield put({type: TRIPS_GET_SUCCESS, trips})
+  } catch (error) {
+    yield put({type: TRIPS_GET_FAIL, error});
+  }
+}
+
+export function *watchTripsRequest() {
+    yield *takeLatest(TRIPS_GET, getTrips);
 }
