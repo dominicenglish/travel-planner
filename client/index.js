821386362a1c59ddd9758b46eca3a8535badb24e
Dominic English <dominic.b.english@gmail.com>
2016-02-22 14:33:05 +1000

    add babel-polyfill, rootReducer, sagas, redux-devtools

--- client/index.js
+++ client/index.js
@@ -1,21 +1,25 @@
+import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from '../shared/routes';
-import { createStore, applyMiddleware, combineReducers } from 'redux';
-import * as reducers from '../shared/redux/reducers/index.js';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+import rootReducer from '../shared/redux/reducers/rootReducer.js';
 import { Provider } from 'react-redux';
 import createSagaMiddleware from 'redux-saga';
-import sagas from '../shared/redux/sagas/sagas.js';
+import * as sagas from '../shared/redux/sagas/sagas.js';
 require('../static/main.css');
 require('../static/bundle.css');
 
 const reduxState = window.__INITIAL_STATE__;
-// const reducer = combineReducers(reducers);
+
 const store = createStore(
-  reducers.counter,
+  rootReducer,
   reduxState,
-  applyMiddleware(createSagaMiddleware(sagas)),
+  compose(
+    applyMiddleware(createSagaMiddleware(sagas.watchTripsRequest)),
+    typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ? window.devToolsExtension() : f => f,
+  ),
 );
 
 render((
