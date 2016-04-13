3f62fc34fbc56909973f0ab69b1d2e0341dd4089
Dominic English <dominic.b.english@gmail.com>
2016-04-13 15:53:06 +1000

    add title and navigation reducers to root reducer

--- shared/redux/reducers/rootReducer.js
+++ shared/redux/reducers/rootReducer.js
@@ -2,8 +2,12 @@ import { routerReducer as routing } from 'react-router-redux';
 import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
 import trips from './tripsReducer.js';
+import title from './titleReducer.js';
+import navigation from './navigationReducer.js';
 
 const rootReducer = combineReducers({
+  navigation,
+  title,
   trips,
   form,
   routing,
