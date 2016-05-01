ec7c05c3f0c947b8e59501d958fb621b04084e5e
Dominic English <dominic.b.english@gmail.com>
2016-05-01 17:12:06 +1000

    add TRIP_GET actions

--- shared/redux/actions/tripsActions.js
+++ shared/redux/actions/tripsActions.js
@@ -3,17 +3,26 @@ export const  TRIPS_GET             = 'TRIPS_GET',
               TRIPS_GET_FAIL        = 'TRIPS_GET_FAIL',
               TRIPS_CREATE          = 'TRIPS_CREATE',
               TRIPS_CREATE_SUCCESS  = 'TRIPS_CREATE_SUCCESS',
-              TRIPS_CREATE_FAIL     = 'TRIPS_CREATE_FAIL'
+              TRIPS_CREATE_FAIL     = 'TRIPS_CREATE_FAIL',
+              TRIP_GET              = 'TRIP_GET',
+              TRIP_GET_SUCCESS      = 'TRIP_GET_SUCCESS',
+              TRIP_GET_FAIL         = 'TRIP_GET_FAIL'
               ;
 
 export const getTrips = () => {
   return {
     type: TRIPS_GET
-  }
+  };
 };
 export const createTrip = (title) => {
   return {
     type: TRIPS_CREATE,
     title,
+  };
+};
+export const getTrip = (id) => {
+  return {
+    type: TRIP_GET,
+    id,
   };
 };
