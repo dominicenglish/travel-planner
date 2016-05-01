f88b040c52f49ef79e03191ddea46f16a3733220
Dominic English <dominic.b.english@gmail.com>
2016-05-01 17:07:30 +1000

    add sagas for STOPS

--- shared/redux/sagas/sagas.js
+++ shared/redux/sagas/sagas.js
@@ -8,6 +8,24 @@ import {
   TRIPS_CREATE,
   TRIPS_CREATE_SUCCESS,
   TRIPS_CREATE_FAIL,
+  TRIP_GET,
+  TRIP_GET_SUCCESS,
+  TRIP_GET_FAIL,
+} from '../actions/tripsActions.js';
+
+import {
+  STOPS_GET,
+  STOPS_GET_SUCCESS,
+  STOPS_GET_FAIL,
+  STOPS_CREATE,
+  STOPS_CREATE_SUCCESS,
+  STOPS_CREATE_FAIL,
+  STOPS_UPDATE,
+  STOPS_UPDATE_SUCCESS,
+  STOPS_UPDATE_FAIL,
+  STOPS_DELETE,
+  STOPS_DELETE_SUCCESS,
+  STOPS_DELETE_FAIL
 } from '../actions/tripsActions.js';
 
 const api = new APIClient();
@@ -30,6 +48,74 @@ function *addTrip() {
   }
 }
 
+function *getTrip(action={}) {
+  const { id } = action;
+  try {
+    const trip = yield call(api.get, `/trips/${id}`);
+    yield put({type: TRIP_GET_SUCCESS, trip});
+  } catch (error) {
+    yield put({type: TRIP_GET_FAIL, error});
+  }
+}
+
+function *getStops(action={}) {
+  const { tripId } = action;
+  try {
+    const stops = yield call(api.get, `/stops`, {params: {tripId}});
+    yield put({type: STOPS_GET_SUCCESS, stops});
+  } catch (error) {
+    yield put({type: STOPS_GET_FAIL, error});
+  }
+}
+
+function *createStop(action={}) {
+  const { tripId, title, description, address, images=[], coordinates={} } = action;
+  try {
+    const stops = yield call(api.create, `/stops`, {
+      params: {
+        tripId,
+        title,
+        description,
+        address,
+        images,
+        coordinates,
+      }
+    });
+    yield put({type: STOPS_CREATE_SUCCESS, stops});
+  } catch (error) {
+    yield put({type: STOPS_CREATE_FAIL, error});
+  }
+}
+
+function *updateStop(action={}) {
+  const { stopId, tripId, title, description, address, images=[], coordinates={} } = action;
+  try {
+    const stop = yield call(api.post, `/stops/${stopId}`, {
+      params: {
+        tripId,
+        title,
+        description,
+        address,
+        images,
+        coordinates,
+      }
+    });
+    yield put({type: STOPS_UPDATE_SUCCESS, stop});
+  } catch (error) {
+    yield put({type: STOPS_UPDATE_FAIL, error});
+  }
+}
+
+function *deleteStop(action={}) {
+  const { stopId } = action;
+  try {
+    const stop = yield call(api.delete, `/stops/${stopId}`);
+    yield put({type: STOPS_DELETE_SUCCESS, stop});
+  } catch (error) {
+    yield put({type: STOPS_DELETE_FAIL, error});
+  }
+}
+
 function *watchTripsRequest() {
     yield *takeLatest(TRIPS_GET, getTrips);
 }
@@ -38,9 +124,33 @@ function *watchAddTrip() {
   yield *takeLatest(TRIPS_CREATE, addTrip);
 }
 
+function *watchGetTrip() {
+  yield *takeLatest(TRIP_GET, getTrip);
+}
+
+function *watchGetStops() {
+  yield *takeLatest(STOPS_GET, getStops);
+}
+
+function *watchCreateStop() {
+  yield *takeLatest(STOPS_CREATE, createStop);
+}
+
+function *watchUpdateStop() {
+  yield *takeLatest(STOPS_UPDATE, updateStop);
+}
+
+function *watchDeleteStop() {
+  yield *takeLatest(STOPS_DELETE, deleteStop);
+}
+
 export default function *root() {
   yield [
     fork(watchTripsRequest),
     fork(watchAddTrip),
+    fork(watchGetStops),
+    fork(watchCreateStop),
+    fork(watchUpdateStop),
+    fork(watchDeleteStop),
   ]
 }
