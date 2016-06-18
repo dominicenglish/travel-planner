b798e44156ff8e0d13e01217fae72412601b154d
Dominic English <dominic.b.english@gmail.com>
2016-06-19 00:36:46 +1000

    fix NAVIGATION_STATE_SET action creator

--- shared/redux/reducers/navigationReducer.js
+++ shared/redux/reducers/navigationReducer.js
@@ -3,7 +3,9 @@ import { NAVIGATION_STATE_SET } from '../actions/navigationActions.js';
 export default (state = false, action = {}) => {
   switch(action.type) {
     case NAVIGATION_STATE_SET:
-      return action.isOpen;
+      if (action.hasOwnProperty('isOpen')) {
+        return action.isOpen;
+      }
     default:
       return state;
   }
