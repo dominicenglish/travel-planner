d911eeeba8b592da46e2677f02b83908e2deb513
Dominic English <dominic.b.english@gmail.com>
2016-06-19 00:30:42 +1000

    update api

--- api/ApiClient.js
+++ api/ApiClient.js
@@ -20,8 +20,8 @@ let fakeApi = {
       password: '4321',
     }
   ],
-  trips: [
-    {
+  trips: {
+    1: {
       id: 1,
       departureDate: '2015-10-01',
       returnDate: '2015-11-01',
@@ -29,8 +29,9 @@ let fakeApi = {
       description: 'A super cool holiday to New Zealand',
       image: 'new_zealand_sheep_farm.jpg',
       users: [1, 2],
+      stops: [1, 2],
     },
-    {
+    2: {
       id: 2,
       departureDate: '2015-05-06',
       returnDate: '2015-05-13',
@@ -38,34 +39,45 @@ let fakeApi = {
       description: 'A week in Melbourne',
       image: 'Road_to_mount_cook_new_zealand.jpg',
       users: [1, 2],
+      stops: [3],
     },
-  ],
-  stops: [
-    {
+  },
+  stops: {
+    1: {
       id: 1,
       tripId: 1,
       title: 'Hobbiton',
       description: 'Hobbiton from LOTR',
       address: '501 Buckland Rd, Hinuera, Matamata 3400, New Zealand',
       images: ['hobbiton.jpg'],
+      details: [
+        {icon: 'phone', label: 'Mobile', value: '0408881111'},
+        {icon: 'email', label: 'Email', value: 'test@testaroo.com'},
+        {icon: 'price', label: 'Cost', value: '150'}
+      ],
       coordinates: {
-        lat: '-37.872513',
-        lng: '175.683291',
+        lat: -37.872513,
+        lng: 175.683291,
       },
     },
-    {
+    2: {
       id: 2,
       tripId: 1,
       title: 'Milford Sound',
       description: 'Cool scenery',
       address: 'Milford Sound, New Zealand',
       images: ['milford-sound.jpg'],
+      details: [
+        {icon: 'phone', label: 'Mobile', value: '0408881111'},
+        {icon: 'email', label: 'Email', value: 'test@testaroo.com'},
+        {icon: 'price', label: 'Cost', value: '150'}
+      ],
       coordinates: {
-        lat: '-44.671892',
-        lng: '167.924120',
+        lat: -44.671892,
+        lng: 167.924120,
       },
     },
-    {
+    3: {
       id: 3,
       tripId: 2,
       title: 'Aquarium!',
@@ -73,15 +85,16 @@ let fakeApi = {
       address: 'Sea Life Melbourne Aquarium, Melbourne',
       images: ['melb-aqua.jpg'],
       coordinates: {
-        lat: '-37.821030',
-        lng: '144.958275',
+        lat: -37.821030,
+        lng: 144.958275,
       },
     },
-  ],
+  },
 };
 
-export default class APIClient {
-  constructor(req) {
+export class APIClient {
+  constructor(dataFile) {
+    this.dataFile = dataFile;
     methods.forEach((method) => {
       this[method] = (path, { params, data } = {}) => new Promise((resolve, reject) => {
         const pathItems = path.split('/');
@@ -100,40 +113,46 @@ export default class APIClient {
   trips(method, filter, params, data) {
     switch (method) {
       case 'get':
-        if (filter) {
-          return fakeApi.trips.find((trip) => {
-            return trip.id == filter;
-          });
+        if (filter.length) {
+          return Object.values(this.dataFile.trips)
+            .find(trip =>  trip.id == filter[0]);
         } else {
-          return fakeApi.trips;
+          return this.dataFile.trips;
         }
       case 'put':
-        // const {trip} = data;
+        const { departureDate, returnDate, title, description, image, users } = params;
+        const tripId = shortid.generate()
         const trip = {
-          id: 3,
-          departureDate: '2017-10-01',
-          returnDate: '2017-11-01',
-          title: 'France',
-          description: 'Francy france france',
-          image: 'new-zealand-short-trip.jpg',
-          users: [1, 2],
+          id: tripId,
+          departureDate,
+          returnDate,
+          title,
+          description,
+          image,
+          users,
         };
-        fakeApi.trips.push(trip);
+        this.dataFile.trips[tripId] = trip;
         return trip;
     }
   }
 
-  stops(method, filter, params, data) {
+  stops(method, filter, params={}, data) {
     switch (method) {
       case 'get':
-        if (filter) {
-          return fakeApi.stops.find(stop => stop.id == filter);
+        if (filter.length) {
+          return Object.values(this.dataFile.stops)
+            .find(stop => stop.id == filter[0]);
         } else if (params.tripId) {
           const { tripId } = params;
-          return fakeApi.stops.filter(stop => stop.tripId == tripId);
+          return Object.values(this.dataFile.stops).reduce((filteredStops, stop) => {
+            if (stop.tripId == tripId) {
+              filteredStops[stop.id] = stop;
+            }
+            return filteredStops;
+          }, {});
         }
       case 'put':
-        const { tripId, title, description, address, images, coordinates } = params;
+        const { tripId='', title='', description='', address='', images=[], coordinates={} } = params;
         const stop = {
           id: shortid.generate(),
           tripId,
@@ -143,30 +162,42 @@ export default class APIClient {
           images,
           coordinates,
         };
-        fakeApi.stops.push(stop);
+        this.dataFile.stops[stop.id] = stop;
         return stop;
 
       case 'post':
-        if (filter) {
-          const { title, description, address, images, coordinates } = params;
-          const index = fakeApi.stops.find(stop => stop.id == filter);
-          Object.assign(fakeApi.stops[index], {
-            title,
-            description,
-            address,
-            images,
-            coordinates}
-          );
+        if (filter.length) {
+          const [stopId] = filter;
+          if (this.dataFile.stops[stopId]) {
+            const { title='', description='', address='', images=[], coordinates={} } = params;
+            return Object.assign(this.dataFile.stops[stopId], {
+              title,
+              description,
+              address,
+              images,
+              coordinates
+            });
+          }
         }
 
       case 'delete':
-        if (filter) {
-          const index = fakeApi.stops.find(stop => stop.id == filter);
-          fakeApi.stops.splice(index, 1);
+        if (filter.length) {
+          const [ stopId ] = filter;
+          if (this.dataFile.stops[stopId]) {
+            const stop = this.dataFile.stops[stopId];
+            delete this.dataFile.stops[stopId];
+            return stop;
+          }
         }
     }
   }
 
 }
 
-// module.exports.getTrips = () => ({trips: data.trips});
+let client;
+const apiClientSingleton = () => {
+  if (!client) client = new APIClient(fakeApi);
+  return client;
+}
+
+export default apiClientSingleton;
