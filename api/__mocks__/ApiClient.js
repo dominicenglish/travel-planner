595263e92cff0d7ebd713911abcbd0d9036a82a8
Dominic English <dominic.b.english@gmail.com>
2016-06-19 00:18:08 +1000

    update mock for ApiClient

--- api/__mocks__/ApiClient.js
+++ api/__mocks__/ApiClient.js
@@ -1,6 +1,16 @@
-const APIClient = jest.genMockFromModule('../ApiClient.js');
-APIClient.default.prototype.get = jest.genMockFunction();
-APIClient.default.prototype.post = jest.genMockFunction();
-APIClient.default.prototype.put = jest.genMockFunction();
-APIClient.default.prototype.delete = jest.genMockFunction();
-export default APIClient.default;
+// const APIClient = jest.genMockFromModule('../ApiClient.js');
+// APIClient.default.prototype.get = jest.genMockFunction();
+// APIClient.default.prototype.post = jest.genMockFunction();
+// APIClient.default.prototype.put = jest.genMockFunction();
+// APIClient.default.prototype.delete = jest.genMockFunction();
+let api = {
+  get: jest.genMockFunction(),
+  post: jest.genMockFunction(),
+  put: jest.genMockFunction(),
+  delete: jest.genMockFunction(),
+}
+const singleton = () => {
+  return api;
+}
+export default singleton;
+// export default APIClient.default;
