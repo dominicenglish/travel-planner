7f6da403a49e65491233f56d6b448b87ec90b31a
Dominic English <dominic.b.english@gmail.com>
2016-06-19 00:44:36 +1000

    add StopInfo component to render a stop info list

---
+++ shared/components/Stops/StopInfo.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import {List, ListItem} from 'material-ui/lib/lists';
+import {
+  CommunicationCall,
+  CommunicationEmail,
+  MapsPlace,
+  EditorAttachMoney
+} from 'material-ui/lib/svg-icons';
+
+const icons = {
+  'phone': <CommunicationCall/>,
+  'email': <CommunicationEmail/>,
+  'price': <EditorAttachMoney/>,
+};
+
+const StopInfo = (props) => {
+  const { address='', details=[] } = props;
+  console.log(props);
+  const detailList = details.map(item => {
+    const icon = icons[item.icon] || <div />;
+    return <ListItem leftIcon={icon} primaryText={item.value} secondaryText={item.label} />;
+  });
+  console.log(detailList);
+  return (
+  <List>
+    <ListItem leftIcon={<MapsPlace/>} primaryText={address} secondaryText="Address"/>
+    {detailList}
+  </List>
+  );
+};
+
+export default StopInfo;
